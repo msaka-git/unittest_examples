5ce0c82bbb7ca859ca4f2b6924821fea713baa1f
msaka-git <43440102+msaka-git@users.noreply.github.com>
2022-03-01 14:13:17 +0100

    update tests

--- test_phonebook.py
+++ test_phonebook.py
@@ -3,10 +3,36 @@ import unittest
 from phonebook import PhoneBook
 
 class PhoneBookTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.phonebook = PhoneBook()
+    
     def test_lookup_by_name(self):
-        phonebook = PhoneBook()
-        phonebook.add("Osman","12345")
+        self.phonebook.add("Osman","12345")
         Number = phonebook.lookup("Osman")
-
         self.assertEqual('12345', Number)
-
+    
+    # We are checking what happens if a name does not exist.
+    # We want to be sure it raises a key error. If return code is True, means test is passed.
+    def test_missing_name(self):
+        with self.assertRaises(KeyError):
+            self.phonebook.lookup("missing")
+    
+    #@unittest.skip('WIP')
+    def test_empty_phonebook_is_consistent(self):
+        self.assertTrue(self.phonebook.is_consistent())
+        
+    def test_is_consistent_with_different_entries(self):
+        self.phonebook.add("Bob","12345")
+        self.phonebook.add("Anna","012345")
+        self.assertTrue(self.phonebook.is_consistent())
+        
+    def test_is_consistent_with_duplicate_entries(self):
+        self.phonebook.add("Bob","12345")
+        self.phonebook.add("Anna","12345")
+        self.assertFalse(self.phonebook.is_consistent())
+        
+    def test_is_consistent_with_duplicate_prefix(self):
+        self.phonebook.add("Bob","12345")
+        self.phonebook.add("Anna","123")
+        self.assertFalse(self.phonebook.is_consistent())
+        
